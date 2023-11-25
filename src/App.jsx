46cf59b1442407837f9942621132e67cc741787b
Jose Garcia <josegarcia19809@gmail.com>
2023-11-24 21:44:02 -0600

    Configurando Tailwind CSS e Instalando React Router DOM 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import AuthLayout from "./layout/AuthLayout.jsx";
+import Login from "./paginas/Login.jsx";
+
 function App() {
 
-  return (
-    <>
-      <h1>Hola Vite y React</h1>
-    </>
-  )
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<AuthLayout/>}>
+                        <Route index element={<Login/>}/>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
 }
 
 export default App

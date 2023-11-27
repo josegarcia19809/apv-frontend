4875ab90220b4e0f71d9726ef2a34b21563a265a
Jgarcia <josegarcia19809@gmail.com>
2023-11-26 19:30:19 -0600

    Creando la página de Registro de Usuarios

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -7,7 +7,7 @@ const Login = () => {
                 <h1 className="text-indigo-600 font-black text-6xl">Inicia sesión y administra
                     tus <span className="text-black">Pacientes</span></h1>
             </div>
-            <div>
+            <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
                 <form>
                     <div className="my-5">
                         <label

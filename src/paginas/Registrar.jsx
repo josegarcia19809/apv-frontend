d100edd936ccf219fad0ad895e0b5c11e07a06bf
Jgarcia <josegarcia19809@gmail.com>
2023-11-29 20:59:55 -0600

    Mostrar Mensajes de Validación

--- src/paginas/Registrar.jsx
+++ src/paginas/Registrar.jsx
@@ -1,5 +1,6 @@
 import {useState} from "react";
 import {Link} from "react-router-dom";
+import Alerta from "../components/Alerta.jsx";
 
 const Registrar = () => {
     const [nombre, setNombre] = useState("");
@@ -7,22 +8,30 @@ const Registrar = () => {
     const [password, setPassword] = useState("");
     const [repetirPassword, setRepetirPassword] = useState("");
 
+    const [alerta, setAlerta] = useState({})
+
     const handleSubmit = e => {
         e.preventDefault();
         if ([nombre, email, password, repetirPassword].includes("")) {
-            console.log("Hay campos vacios");
+            setAlerta({msg: "Hay campos vacios", error: true});
             return;
         }
         if (password !== repetirPassword) {
-            console.log("Los passwords no son iguales");
+            setAlerta({msg: "Los passwords no son iguales", error: true});
             return;
         }
         if (password.length < 6) {
-            console.log("El password es muy corto, agrega mínimo 6 caracteres");
+            setAlerta({
+                msg: "El password es muy corto, agrega mínimo 6 caracteres",
+                error: true
+            });
             return;
         }
+        setAlerta({});
 
+        // Crear el usuario en la API
     }
+    const {msg} = alerta
     return (
         <>
             <div>
@@ -33,6 +42,11 @@ const Registrar = () => {
 
             </div>
             <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
+                {
+                    msg && <Alerta
+                        alerta={alerta}/>
+                }
+
                 <form
                     onSubmit={handleSubmit}
                 >

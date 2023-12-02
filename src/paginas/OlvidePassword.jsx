f6ec8f4270eed471379652ea6dd6bcf4ec653727
Jose Garcia <josegarcia19809@gmail.com>
2023-12-01 22:12:58 -0600

    Generando Token y Validación del Token único para cambiar el password

--- src/paginas/OlvidePassword.jsx
+++ src/paginas/OlvidePassword.jsx
@@ -1,6 +1,35 @@
 import {Link} from "react-router-dom";
+import {useState} from "react";
+import Alerta from "../components/Alerta.jsx";
+import clienteAxios from "../config/axios.jsx";
 
 const OlvidePassword = () => {
+    const [email, setEmail] = useState('');
+    const [alerta, setAlerta] = useState({});
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (email === "" || email.length < 6) {
+            setAlerta({
+                msg: "El email es obligatorio",
+                error: true
+            });
+            return;
+        }
+        try {
+            const {data} = await clienteAxios.post('/veterinarios/olvide-password',
+                {email});
+            setAlerta({msg: data.msg});
+        } catch (e) {
+            setAlerta({
+                msg: e.response.data.msg,
+                error: true
+            })
+        }
+    }
+    const {msg} = alerta;
+
     return (
         <>
             <div>
@@ -11,8 +40,13 @@ const OlvidePassword = () => {
 
             </div>
             <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
+                {
+                    msg && <Alerta
+                        alerta={alerta}
+                    />
+                }
                 <form
-
+                    onSubmit={handleSubmit}
                 >
 
                     <div className="my-5">
@@ -25,7 +59,8 @@ const OlvidePassword = () => {
                             type="email"
                             placeholder="Email de Registro"
                             className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
-
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
                         />
                     </div>
 

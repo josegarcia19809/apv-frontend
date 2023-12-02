72fbe77bc966bdfb61f4155851ce99fb4746f0eb
Jose Garcia <josegarcia19809@gmail.com>
2023-12-01 21:28:53 -0600

    Creando una URLBase en Axios

--- src/paginas/ConfirmarCuenta.jsx
+++ src/paginas/ConfirmarCuenta.jsx
@@ -1,6 +1,6 @@
 import {useEffect, useState} from "react";
 import {useParams, Link} from "react-router-dom";
-import axios from "axios";
+import clienteAxios from "../config/axios.jsx";
 import Alerta from "../components/Alerta.jsx";
 
 const ConfirmarCuenta = () => {
@@ -13,8 +13,8 @@ const ConfirmarCuenta = () => {
     useEffect(() => {
         const confirmarCuenta = async () => {
             try {
-                const url = `${import.meta.env.VITE_BACKEND_URL}/api/veterinarios/confirmar/${id}`;
-                const {data} = await axios(url);
+                const url = `veterinarios/confirmar/${id}`;
+                const {data} = await clienteAxios(url);
                 console.log(data);
                 setCuentaConfirmada(true);
                 setAlerta({

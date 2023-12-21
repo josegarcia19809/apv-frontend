8a974dfa751a677c2568beb7791e8c966910399d
Jose Garcia <josegarcia19809@gmail.com>
2023-12-21 16:32:47 -0600

    Creando el Header de nuestro Proyecto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Header = () => {
     return (
-        <>
-            <h1>Header</h1>
-        </>
+        <header className="py-10 bg-indigo-600">
+            <div className="container mx-auto flex justify-between items-center">
+                <h1 className="font-bold text-2xl text-indigo-200">
+                    Administrador de pacientes de {""}
+                    <span className="text-white font-black">Veterinaria</span>
+                </h1>
+                <nav className="flex gap-4">
+                    <Link to="/admin" className="text-white text-sm uppercase font-bold">
+                        Pacientes</Link>
+                    <Link to="/perfil" className="text-white text-sm uppercase font-bold">
+                        Perfil</Link>
+                    <button
+                        type="button"
+                        className="text-white text-sm uppercase font-bold"
+                    >
+                        Cerrar sesión
+                    </button>
+                </nav>
+            </div>
+
+        </header>
     );
 };
 

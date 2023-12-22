dc73416a64c3f0506951e488d53ef6c170b005b9
Jose Garcia <josegarcia19809@gmail.com>
2023-12-22 10:06:13 -0600

    Finalizando el Componente para Mostrar Pacientes

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 const Paciente = ({paciente}) => {
     const {email, fecha, nombre, propietario, sintomas, _id} = paciente
+
+    const formatearFecha = (fecha) => {
+        const nuevaFecha = new Date(fecha);
+        return new Intl.DateTimeFormat('es-MX', {dateStyle: 'long'}).format(nuevaFecha);
+    }
+    
     return (
         <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
             <p className="font-bold uppercase text-indigo-700 my-2">Nombre: {''}
@@ -14,8 +20,9 @@ const Paciente = ({paciente}) => {
                 <span className="font-normal normal-case text-black">{email}</span>
             </p>
             <p className="font-bold uppercase text-indigo-700 my-2">Fecha de Alta: {''}
-                <span
-                    className="font-normal normal-case text-black">{fecha}</span>
+                <span className="font-normal normal-case text-black">
+                    {formatearFecha(fecha)}
+                </span>
             </p>
             <p className="font-bold uppercase text-indigo-700 my-2">Síntomas: {''}
                 <span className="font-normal normal-case text-black">{sintomas}</span>
